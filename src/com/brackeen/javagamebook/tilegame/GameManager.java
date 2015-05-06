01dbe028db8edc9a68d957f7dcc91d75ae289eb2
Melylennon95 <melylennon@gmail.com>
2015-05-06 01:46:26 -0500

    Perdon ahaha ahora si ulrimo music update

--- src/com/brackeen/javagamebook/tilegame/GameManager.java
+++ src/com/brackeen/javagamebook/tilegame/GameManager.java
@@ -1095,7 +1095,7 @@ public class GameManager extends GameCore {
                 }
                 case 7:{
                    tmrRenderer.setBackground(lklBackgrounds.get(7));
-                   mpMidiPlayer.play(seqSequence3, true);
+                   mpMidiPlayer.play(seqSequence4, true);
                     break;
                 }
                 case 8:{
